a2a6ec647054a8a91f9e2ff05efe1390fe7cb17e
Ethan Brierley <ethanboxx@gmail.com>
2019-09-13 08:19:46 +0100

    ?join command

--- bot.py
+++ bot.py
@@ -24,9 +24,23 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    welcome_room = discord.utils.get(guild.channels, name="welcome-room")
+    await welcome_room.send("Hi")
+
+
+@client.event
+async def on_message(message):
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    if message.author == client.user:
+        return
+
+    print(message.channel.name)
+
+    if message.content.lower() == '?join':
+        await message.channel.send("Hi")
+        role = discord.utils.get(guild.roles, name="member")
+        await message.author.add_roles(role)
+
 
 client.run(token)
