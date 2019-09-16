5ad75c22931528fb14638714694b11aaa848ac5d
Ethan Brierley <ethanboxx@gmail.com>
2019-09-16 18:25:54 +0100

    end ping

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 
 cd /
 cd home/pi/Documents/bot/esce-students-discord-bot
-ping www.google.com -c 4
+# ping www.google.com -c 4
 sleep 30
-ping www.google.com -c 4
+# ping www.google.com -c 4
 python3 bot.py
