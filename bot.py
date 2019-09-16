28bb47367763d21f9e7d5f9fe30a59de82f5f415
Ethan Brierley <ethanboxx@gmail.com>
2019-09-16 12:31:29 +0100

    start working roles

--- bot.py
+++ bot.py
@@ -27,29 +27,33 @@ async def on_member_join(member):
     guild = discord.utils.get(client.guilds, name=GUILD)
     welcome_room = discord.utils.get(guild.channels, name="welcome-room")
     # maybe post reminder message to non member users ever so often
-    await welcome_room.send("Hello and welcome " + member.mention + " Thank you for joining" + """
+    await welcome_room.send("Hello and welcome " + member.mention + " Thank you for joining!" + """
 We use bots on this server. Please follow the steps to gain access to the channels.
 It is important we know who you are so we are going to need to change your nickname to the name you use or plan to use at ESC
-Type `?username your name` using your name. For example in my case I would type,
+Type `?student your name` using your name. For example in my case I would type,
 ```
-?username Ethan Brierley
+?student Ethan Brierley
 ```""")
 
 
 @client.event
 async def on_message(message):
     guild = discord.utils.get(client.guilds, name=GUILD)
+    bot_commands = discord.utils.get(guild.channels, name="bot-commands")
     if message.author == client.user:
         return
 
     print(message.channel.name)
 
     if message.channel.name == "welcome-room":
-        username = re.search('\?username .*', message.content)
+        username = re.search('\?student .*', message.content)
         if username != None:
-            username = username.group(0)[10:]
+            username = username.group(0)[9:]
             user = message.author
             await user.edit(nick=username)
+            role = discord.utils.get(guild.roles, name="member")
+            await message.author.add_roles(role)
+            await bot_commands.send(user.mention + " I have changed your nickname to " + username)
     elif message.content.lower() == '?join':
         await message.channel.send("Hi")
         role = discord.utils.get(guild.roles, name="member")
