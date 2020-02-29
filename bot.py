27dcb482f3a5b4646744f10c4d34b0582114198b
ArmanSattar <armanscsgo@gmail.com>
2020-02-29 19:39:55 +0000

    Move variables into the main program

--- bot.py
+++ bot.py
@@ -12,6 +12,28 @@ client = discord.Client()
 
 A_LEVEL_SUBJECTS = ["maths", "physics", "computer-science",
                     "further-maths", "chemistry", "biology", "accounting"]
+eResponses = [
+    'As I see it, yes.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    'Don’t count on it.',
+    'It is certain.',
+    'It is decidedly so.',
+    'Most likely.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Outlook good.',
+    'Reply hazy, try again.',
+    'Signs point to yes.',
+    'Very doubtful.',
+    'Without a doubt.',
+    'Yes.',
+    'Yes – definitely.',
+    'You may rely on it.'
+    ]
 BTEC_SUBJECTS = ["art"]
 
 
